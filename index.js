2025ec31f134d6ff65de1ef331e4629d175b5708
AllCirclesPresuppose <114363778+AllCirclesPresuppose@users.noreply.github.com>
2023-04-19 21:41:59 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const posts = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "images/avatar-vangogh.jpg",
+        post: "images/post-vangogh.jpg",
+        comment: "just took a few mushrooms lol",
+        likes: 21
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "images/avatar-courbet.jpg",
+        post: "images/post-courbet.jpg",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 4
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "images/avatar-ducreux.jpg",
+        post: "images/post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152
+    }
+]
+
+const everything = document.getElementById("everything")
+let allPosts
+
+for (let i = 0; i < posts.length; i++) {
+    
+}
+// [main] avatar (main class "users"),{class flex} name (main h2), location (main p){/class}; [article] post (class post) [footer]{class interact 3 icons} likes (footer h3), username (class "comments" h3), comment (class comments p)
